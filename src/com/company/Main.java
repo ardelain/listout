c882568acc10a8d5a842a3b2497107c6202fd0ec
ardelain <delainarthur@gmx.fr>
2018-11-22 21:11:56 +0100

    test

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main { //ya des problemes avec spark sur des version de intelij, j'ai galere ^^ enfin bon j'ai ajouter les librairies a la main au final
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("**********************************************************************");
+        System.out.println("**********************************************************************");
+        System.out.println("\t\t\t\t\t\tLISTOUT DEMARRAGE :");
+        System.out.println("**********************************************************************");
+        System.out.println("**********************************************************************");
+    }
+}
